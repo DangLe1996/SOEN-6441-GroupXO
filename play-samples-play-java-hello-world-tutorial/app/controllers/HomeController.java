ef63b04844161e69f34e2dd728a1dcd5809dd0e6
GirishKadapa <kadapagirish@gmail.com>
2020-10-31 15:02:30 -0400

    Girish - Updated the tweet pricessing with parallel streams, refactored session management, view
    Girish - Updated the tweet pricessing with parallel streams, refactored session management, view

--- play-samples-play-java-hello-world-tutorial/app/controllers/HomeController.java
+++ play-samples-play-java-hello-world-tutorial/app/controllers/HomeController.java
@@ -15,7 +15,9 @@ import play.mvc.*;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -38,16 +40,16 @@ public class HomeController extends Controller {
 	
 	private class session_data{
 		private List<String> queries;
-	    private LinkedHashMap<String,List<Status>> cache;
+	    private LinkedHashMap<String,String> cache;
 		
-		session_data(List<String> query,LinkedHashMap<String,List<Status>> cache){
+		session_data(List<String> query,LinkedHashMap<String,String> cache){
 			this.queries = query;
 			this.cache = cache;
 		}
 		private List<String> getQuery(){
 			return this.queries;
 		}
-		private LinkedHashMap<String,List<Status>> getCache(){
+		private LinkedHashMap<String,String> getCache(){
 			return this.cache;
 		}
 		
@@ -62,7 +64,6 @@ public class HomeController extends Controller {
     public HomeController(FormFactory formFactory, MessagesApi messagesApi) {
         this.form = formFactory.form(Search.class);
         this.messagesApi = messagesApi;
-        //this.cache = new LinkedHashMap<String, List<Status>>();
         this.session_count = 0;
         this.sessions =  new HashMap<String,session_data>();
     }
@@ -71,41 +72,47 @@ public class HomeController extends Controller {
     	final Form<Search> boundForm = form.bindFromRequest(request);
 
         if (boundForm.hasErrors()) {
-            return CompletableFuture.completedFuture(badRequest(views.html.tweets_display.render(new ArrayList<String>(),new LinkedHashMap<String,List<Status>>(), boundForm, request, messagesApi.preferred(request))));
+            return CompletableFuture.completedFuture(badRequest(views.html.tweets_display.render(new ArrayList<String>(),new LinkedHashMap<String,String>(), boundForm, request, messagesApi.preferred(request))));
         } else {
             Search searchquery = boundForm.get();
              
 	        String current_user = request.session().get("Twitter").get(); 
-	        session_data current_session = this.sessions.get(current_user);
-	
-	        if (current_session != null) {
-	
-		        LinkedHashMap<String,List<Status>> cache = current_session.getCache();	
-		        boolean alreadySearched = cache.containsKey(searchquery.getSearchString());  
-		        if (alreadySearched) {  
+	        session_data current_session = getSession(current_user,false);
+	        LinkedHashMap<String,String> cache = current_session.getCache();	
+	        List<String> query = current_session.getQuery();
+		    boolean alreadySearched = cache.containsKey(searchquery.getSearchString());  
+		    if (alreadySearched) {  
+		    		query.remove(searchquery.getSearchString());
+		    		query.add(0,searchquery.getSearchString());
 		        	return CompletableFuture.completedFuture(redirect(routes.HomeController.searchPage())
 		        			.addingToSession(request, "Twitter", current_user)
 		        			);
-		        }
-		        else {
-		        	List<String> query = current_session.getQuery();
-		        	query.add(searchquery.getSearchString()); 
-			        return (GetTweets.GetTweets_keyword(searchquery.getSearchString())
-			        	   .thenApply(status -> {cache.put(searchquery.getSearchString(),status);
+		    }
+		    else {
+		        query.add(0,searchquery.getSearchString()); 
+			    return new GetTweets().GetTweets_keyword(searchquery.getSearchString())
+			        	   .thenApply(tweet -> {tweet = "		<tr>\n" + 
+								        				"			<th>Search terms:</th>\n" + 
+								        				"			<th><a href=/keyword?s=" + searchquery.getSearchString().replaceAll(" ", "+") + "'>" + searchquery.getSearchString() + "</a></th>\n" + 
+								        				"		</tr>\n" + 
+								        				"		<tr>\n" + 
+								        				"			<th>User</th>\n" + 
+								        				"			<th>Location</th>\n" + 
+								        				"			<th>Tweet Text</th>\n" + 
+								        				"		</tr>\n" + tweet;
+			        		   					cache.put(searchquery.getSearchString(),tweet);
 			        	   						return redirect(routes.HomeController.searchPage())
 			        	   								.addingToSession(request, "Twitter", current_user)
-			        	   								;}));
-			        
-		        }
-	        }
-	        else {
-	        	return CompletableFuture.completedFuture(redirect(routes.HomeController.searchPage())
-	        			.addingToSession(request, "Twitter", current_user)
-	        			);
-	         	
-	        }
-        }
-    
+			        	   								;});
+			    /* if we have to add keyword hyperlink at the begining, then i couldnt find any way other than making it sequntial 
+		         * and then reduce with initial value like below, but we will loose performance of parallel stream
+		         * 
+		         * another way to do is in controller, after tweets are fetched add in the begining but this will breach
+		         * no business logic in controller rule
+		         */     
+		    }
+
+        }      
     }
     
     public CompletionStage<Result> searchPage(Http.Request request) {
@@ -114,51 +121,59 @@ public class HomeController extends Controller {
     		      .session()
     		      .get("Twitter")
     		      .map(user -> { 
-    		    	  			String current_user = request.session().get("Twitter").get();
-    		    	  			session_data current_session = this.sessions.get(current_user);
-			        			if (current_session != null) {
-				        			List<String> query = current_session.getQuery();
-				        			LinkedHashMap<String,List<Status>> cache = current_session.getCache();
-				        			return CompletableFuture.completedFuture(ok(views.html.tweets_display.render(query,cache , form, request, messagesApi.preferred(request)))
-				        					.addingToSession(request, "Twitter", current_user)
-				        					)
-				       				     ;
-				        			}
-			        			else {
-				        				List<String> query = new ArrayList<String>();;
-				            			LinkedHashMap<String,List<Status>> cache = new LinkedHashMap<String, List<Status>>();
-				            			return CompletableFuture.completedFuture(ok(views.html.tweets_display.render(query,cache , form, request, messagesApi.preferred(request)))
-				            					.addingToSession(request, "Twitter", current_user)
-				           				     );
-				        			}
-        			
+    		    	  			session_data current_session = getSession(user,false);
+				        		return CompletableFuture.completedFuture(ok(views.html.tweets_display.render(current_session.getQuery(),current_session.getCache(), form, request, messagesApi.preferred(request)))
+				        					.addingToSession(request, "Twitter", user)
+				        					);       			
         						})
     		      .orElseGet(() ->{
-					        	 LinkedHashMap<String,List<Status>> newcache =  new LinkedHashMap<String, List<Status>>();
-					        	 List<String> newquery = new ArrayList<String>();
-					    		 List<Object> newsession = new ArrayList<Object>();
-					    		 newsession.add(newquery);
-					    		 newsession.add(newcache);
-					    		 this.session_count += 1;
-					    		 session_data s = new session_data(newquery,newcache);
-					    		 String newuser = "play" + Integer.toString(this.session_count);
-					    		 this.sessions.put(newuser,s);
-					    		 return CompletableFuture.completedFuture(ok(views.html.tweets_display.render(newquery,newcache , form, request, messagesApi.preferred(request)))
+		    					 this.session_count += 1;
+		    					 String newuser = "play" + Integer.toString(this.session_count);
+    		    	  			 session_data new_session = getSession(newuser,true);
+					    		 return CompletableFuture.completedFuture(ok(views.html.tweets_display.render(new_session.getQuery(),new_session.getCache(), form, request, messagesApi.preferred(request)))
 					    				.addingToSession(request, "Twitter", newuser));
     		      				});
     }
+    
+    private session_data getSession(String user,boolean newsession) {
+    	if (newsession) {
+	    	LinkedHashMap<String,String> newcache =  new LinkedHashMap<String, String>();
+	   	 	List<String> newquery = new ArrayList<String>();
+			session_data new_session = new session_data(newquery,newcache);
+			this.sessions.put(user,new_session);
+			return new_session;
+    	}
+    	else {
+    		session_data current_session = this.sessions.get(user);
+    		if (current_session != null) {
+    			return current_session;
+    		}
+    		else {
+    			List<String> query = new ArrayList<String>();;
+    			LinkedHashMap<String,String> cache = new LinkedHashMap<String, String>();
+    			session_data s = new session_data(query,cache);
+    			this.sessions.put(user,s);
+    			return s;
+    		}
+    	}
+    }
 
-    public Result user(String g) {
+    public Result user(Http.Request request,String g) {
     	return ok("in user " + g);
     	
     }
-    public Result location(String g) {
+    public Result location(Http.Request request,String g) {
     	return ok("in location " + g);
     	
     }
-    public Result word(String g) {
-    	return ok("in word " + g);
+    public Result keyword(Http.Request request,String g) {
+    	return ok("in keyword " + g);
     	
     }
+    public Result hashtag(Http.Request request,String g) {
+    	return ok("in hashtag " + g);
+    	
+    }
+    
 
 }
--- play-samples-play-java-hello-world-tutorial/app/models/GetTweets.java
+++ play-samples-play-java-hello-world-tutorial/app/models/GetTweets.java
@@ -1,5 +1,7 @@
 package models;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -18,7 +20,7 @@ public class GetTweets {
 
 	}
 	
-	public static CompletionStage<List<Status>> GetTweets_keyword(String keyword) throws TwitterException{	
+	public static CompletionStage<String> GetTweets_keyword(String keyword) throws TwitterException{	
         if (keyword.length() < 1) {
             System.exit(-1);
         }
@@ -30,8 +32,47 @@ public class GetTweets {
 
         return CompletableFuture.completedFuture(twitter.search(query).getTweets()
 				.parallelStream()
-				.collect(Collectors.toList()));
+				.map(s -> {
+					return "\n" +
+						   "<tr>\n" + 
+							"		<td><a href=/user?s=" + s.getUser().getScreenName().replaceAll(" ", "+") + "> " + s.getUser().getScreenName() + "</a></td>\n" + 
+							"		<td><a href=/location?s=" + s.getUser().getLocation().replaceAll(" ", "+") + ">" + s.getUser().getLocation() + "</a></td>\n" + 
+							"		<td>" + s.getText().replaceAll("#(\\w+)+", "<a href=/hashtag?s=$1>#$1</a>") + "</td>\n" +
+							"</tr>\n"; 
+						})
+        		.reduce("",
+        				String::concat));
+        
+        /* There are 2 ways of doing this as per me,
+         * if we have to add keyword hyperlink at the begining, then i couldnt find any way other than making it sequntial 
+         * and then reduce with initial value like below, but we will loose performance of parallel stream
+         * 
+         * another way to do is in controller, after tweets are fetched add in the begining but this will breach
+         * no business logic in controller rule
+         *         return CompletableFuture.completedFuture(twitter.search(query).getTweets()
+				.parallelStream()
+				.map(s -> {
+					return "\n" +
+						   "<tr>\n" + 
+							"		<td><a href=/user?s=" + s.getUser().getScreenName().replaceAll(" ", "+") + "> " + s.getUser().getScreenName() + "</a></td>\n" + 
+							"		<td><a href=/location?s=" + s.getUser().getLocation().replaceAll(" ", "+") + ">" + s.getUser().getLocation() + "</a></td>\n" + 
+							"		<td>" + s.getText().replaceAll("#(\\w+)+", "<a href=/hashtag?s=$1>#$1</a>") + "</td>\n" +
+							"</tr>\n"; 
+						})
+				.sequential()
+        		.reduce("		<tr>\n" + 
+        				"			<th>Search terms:</th>\n" + 
+        				"			<th><a href=/keyword?s=" + keyword.replaceAll(" ", "+") + "'>" + keyword + "</a></th>\n" + 
+        				"		</tr>\n" + 
+        				"		<tr>\n" + 
+        				"			<th>User</th>\n" + 
+        				"			<th>Location</th>\n" + 
+        				"			<th>Tweet Text</th>\n" + 
+        				"		</tr>\n",
+        				String::concat));
+         */
 
     }
 
 }
+	
