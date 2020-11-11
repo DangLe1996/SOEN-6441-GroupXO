abd0ca119c34720f59e01c50629db3f66b70f906
suhelm <suhelfirdus@gmail.com>
2020-11-11 17:26:52 -0500

    100%

--- SOEN-6441-Project/test/models/SearchTest.java
+++ SOEN-6441-Project/test/models/SearchTest.java
@@ -2,38 +2,110 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import play.Application;
+import play.inject.guice.GuiceApplicationBuilder;
+import play.test.WithApplication;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-
-class SearchTest {
-
+import static org.junit.jupiter.api.Assertions.*;
 
 
-    @Test
-    void testCorrectString() {
-        String searchPhrase = "this is test search";
-        Search testSearch = new Search();
-        testSearch.setSearchString(searchPhrase);
-        assertThat(testSearch.getSearchString(),is(searchPhrase));
+import static org.hamcrest.CoreMatchers.*;
+
+public class sessionDataTest  extends WithApplication {
+
+    @Override
+    protected Application provideApplication() {
+        return new GuiceApplicationBuilder().build();
+    }
+
+
+    private static sessionData testUser;
+    //@BeforeEach
+    public void setUp() {
+        for(int i = 0; i < 3; i++){
+            new sessionData();
+        }
+        testUser =  new sessionData();
+    }
+
+    //@AfterEach
+    public  void tearDown() {
+
+        sessionData.cleanUpSessions();
 
     }
 
     @Test
-    void testEmptyString() {
-        String searchPhrase = " ";
-        Search testSearch = new Search();
-        testSearch.setSearchString(searchPhrase);
-        assertThat(testSearch.getSearchString(),is(searchPhrase));
+    public  void getTestUser() {
+        setUp();
 
+        assertThat(sessionData.userCache.size(),is(4));
+        assertThat(sessionData.getUser(testUser.toString()),is(equalTo(testUser)));
+        tearDown();
+    }
+
+    @Test
+    public  void getInvalidUser(){
+        setUp();
+        assertThat(sessionData.getUser(null).getSessionID(),is("play5"));
+        assertThat(sessionData.getUser("badID").getSessionID(),is("play6"));
+        tearDown();
     }
     @Test
-    void testNullString() {
-        String searchPhrase = null;
-        Search testSearch = new Search();
-        testSearch.setSearchString(searchPhrase);
-        assertThat(testSearch.getSearchString(),is(searchPhrase));
-
+    public  void testToString() {
+        setUp();
+        assertThat(testUser.toString(),is("sessionData{" +
+                "sessionID='" + "play4" + '\'' +
+                '}'));
+        tearDown();
     }
+
+
+    @Test
+    public  void insertNewCache() {
+        setUp();
+
+        testUser.insertCache("test1","this is test 1");
+        testUser.insertCache("test2","this is test 2");
+        testUser.insertCache("test3","this is test 3");
+
+        assertThat(testUser.getCache().size(),is(3));
+        assertThat(testUser.getQuery().size(),is(3));
+        tearDown();
+    }
+    
+    @Test
+    public  void insertCacheWithExisting(){
+        setUp();
+        testUser.insertCache("test2","this is test 2");
+        testUser.insertCache("test2","this is test 2.1");
+
+        assertThat(testUser.getCache().size(),is(1));
+        assertThat(testUser.getQuery().size(),is(1));
+        tearDown();
+    }
+
+    @Test
+    public void testCacheMaxSize(){
+        setUp();
+
+        //sessionData testUser=new sessionData() ;
+        for(int i = 0; i < 12; i++){
+            String keyword = "test" + i;
+            String result = "this is test " + i;
+            testUser.insertCache(keyword,result);
+        }
+        assertThat(testUser.getCache().size(),is(10));
+        assertThat(testUser.getQuery().size(),is(10));
+        assertThat(testUser.getQuery().get(0),is("test11"));
+        assertThat(testUser.getCache().get("test11"),is("this is test 11"));
+        tearDown();
+    }
+
+
+
+
 }
