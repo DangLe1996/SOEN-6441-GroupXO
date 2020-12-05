a994ebe7f7660ddd0b28a957720f14d42d0910cd
suhelm <suhelfirdus@gmail.com>
2020-12-05 14:29:25 -0500

    Moved reply class as public

--- SOEN-6441-Project/app/actors/SentimentActor.java
+++ SOEN-6441-Project/app/actors/SentimentActor.java
@@ -19,7 +19,7 @@ public class SentimentActor extends AbstractActor {
         this.replyTo = replyTo;
     }
 
-    public static class tweetsSet{
+    /*public static class tweetsSet{
         private final String queryTerm;
         private final List<Status> allTweets;
 
@@ -27,7 +27,7 @@ public class SentimentActor extends AbstractActor {
             this.queryTerm = queryTerm;
             this.allTweets=allTweets;
         }
-    }
+    } */
     public static class tweetStatus{
         private final Status status;
         private final String searchString;
@@ -37,6 +37,19 @@ public class SentimentActor extends AbstractActor {
             this.searchString=searchString;
         }
     }
+
+    public class storeSentiments {
+        public String keyword;
+        public long msgID;
+        public String mode;
+
+        public storeSentiments(String keyword, long msgID, String mode) {
+            this.keyword = keyword;
+            this.msgID = msgID;
+            this.mode = mode;
+        }
+    }
+
 
     public static Props props(ActorRef ws, ActorRef replyTo) {
         return Props.create(SentimentActor.class, () -> new SentimentActor( ws, replyTo));
@@ -49,8 +62,11 @@ public class SentimentActor extends AbstractActor {
 
                     String mode=getTweetSentimentsPerTweets(msg.status);
 
-                    ws.tell(new TwitterStreamActor.storeSentiments(msg.searchString,msg.status.getId(),mode),getSelf());
+                    getSender().tell(new storeSentiments(msg.searchString,msg.status.getId(),mode),getSelf());
                 })
+                .matchEquals("KillSwitch", msg -> {
+                    System.out.println("Actor terminated");
+                    context().stop(self());})
                 .matchAny(msg -> {
                     System.out.println("got the Bunch of Tweets");
                 })
@@ -65,7 +81,7 @@ public class SentimentActor extends AbstractActor {
 
     private String getTweetSentimentsPerTweets(Status singleTweets){
 
-        System.out.println("Single Tweets Analysis");
+        //System.out.println("Single Tweets Analysis");
         if (stringContainsItemFromList(singleTweets.getText(), happy))
             return GetTweets.Mode.HAPPY.toString();
         else if (stringContainsItemFromList(singleTweets.getText(), sad))
@@ -91,7 +107,7 @@ public class SentimentActor extends AbstractActor {
      * User Defined array of words which when present marks a tweet as sad
      */
     private final static String[] sad = {"SAD", "ANGRY", ":(", "MAD", "DISAPPOINTMENT", "BAD DAY","\uD83D\uDCCA","\uD83D\uDE1E","\uD83D\uDE14"};
-  /**
+    /**
      * enum of mode of a tweet HAPPY, SAD , NEUTRAL
      */
     public enum Mode {
